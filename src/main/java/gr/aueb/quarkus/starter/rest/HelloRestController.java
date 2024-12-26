f34a46d2e1e96235091e404b3f7477226b0f4111
Siberian <vasileios.kritikos@aueb.gr>
2024-12-26 16:20:34 +0200

    HelloRestController

--- src/main/java/gr/aueb/quarkus/starter/rest/HelloRestController.java
+++ src/main/java/gr/aueb/quarkus/starter/rest/HelloRestController.java
@@ -1,11 +1,10 @@
 package gr.aueb.quarkus.starter.rest;
 
+import gr.aueb.quarkus.starter.dto.StudentReadOnlyDTO;
 import gr.aueb.quarkus.starter.dto.TeacherReadOnlyDTO;
 import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +28,18 @@ public class HelloRestController {
         if(Objects.equals("id" , 1L)){
             return new ResponseEntity<>(readOnlyDTO, HttpStatus.OK);
         } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/students/{id}")
+    public ResponseEntity<StudentReadOnlyDTO> getOneStudent(@PathParam("id") Long id){
+
+        StudentReadOnlyDTO studentReadOnlyDTO = new StudentReadOnlyDTO(1L, "Alice" , "Wonderland","Chemistry");
+
+        if (Objects.equals("id" , 1L)){
+            return new ResponseEntity<>(studentReadOnlyDTO , HttpStatus.OK);
+        }else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
